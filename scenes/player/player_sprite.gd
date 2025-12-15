891d784ce8e8f6b367226ba625daa4c6f4e2767d
Evan Johnson <johnsonev207@gmail.com>
2025-12-14 22:07:45 -0800

    Add player silhouette, change damage/heal/death shader behavior

---
+++ scenes/player/player_sprite.gd
@@ -0,0 +1,34 @@
+extends Sprite2D
+
+@onready var silhouette: Sprite2D = $Silhouette
+
+
+func _ready():
+	silhouette.flip_h = flip_h
+	silhouette.hframes = hframes
+	silhouette.offset = offset
+	silhouette.texture = texture
+	silhouette.vframes = vframes
+
+	# Has to go after hframes and vframes are set.
+	silhouette.frame = frame
+
+	silhouette.visible = true
+
+
+func _set(property: StringName, value: Variant) -> bool:
+	match property:
+		"flip_h":
+			silhouette.flip_h = value
+		"frame":
+			silhouette.frame = value
+		"hframes":
+			silhouette.hframes = value
+		"offset":
+			silhouette.offset = value
+		"texture":
+			silhouette.texture = value
+		"vframes":
+			silhouette.vframes = value
+
+	return false
