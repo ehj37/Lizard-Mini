8b6b6c96ea6f5b0bf5413078dc6ff4a5e90b42a6
Evan Johnson <johnsonev207@gmail.com>
2025-12-17 21:10:28 -0800

    Add camera lock area

---
+++ scenes/camera_lock_area/camera_lock_area.gd
@@ -0,0 +1,62 @@
+@tool
+
+class_name CameraLockArea
+
+extends Area2D
+
+const COLOR_RECT_A := 0.05
+const COLOR_X_ONLY := Color.RED
+const COLOR_Y_ONLY := Color.GREEN
+const COLOR_X_AND_Y := Color.PURPLE
+const COLOR_NEITHER_X_OR_Y := Color.YELLOW
+
+@export var player_camera: PlayerCamera
+
+@export var lock_x := true:
+	set(new_lock_x):
+		lock_x = new_lock_x
+		if Engine.is_editor_hint() && is_node_ready():
+			_set_color()
+
+@export var lock_y := true:
+	set(new_lock_y):
+		lock_y = new_lock_y
+		if Engine.is_editor_hint() && is_node_ready():
+			_set_color()
+
+@onready var color_rect: ColorRect = $ColorRect
+
+
+func _ready():
+	assert(player_camera != null, "Must provide player camera export var")
+
+	if Engine.is_editor_hint():
+		var viewport_width = ProjectSettings.get_setting("display/window/size/viewport_width")
+		var viewport_height = ProjectSettings.get_setting("display/window/size/viewport_height")
+		color_rect.set_size(Vector2(viewport_width, viewport_height))
+		color_rect.set_position(Vector2(-viewport_width / 2, -viewport_height / 2))
+
+		_set_color()
+	else:
+		color_rect.visible = false
+
+
+func _on_body_entered(_body) -> void:
+	player_camera.register_lock_area(self)
+
+
+func _on_body_exited(_body) -> void:
+	player_camera.unregister_lock_area(self)
+
+
+func _set_color() -> void:
+	if lock_x && !lock_y:
+		color_rect.color = COLOR_X_ONLY
+	elif lock_y && !lock_x:
+		color_rect.color = COLOR_Y_ONLY
+	elif lock_x && lock_y:
+		color_rect.color = COLOR_X_AND_Y
+	else:
+		color_rect.color = COLOR_NEITHER_X_OR_Y
+
+	color_rect.color.a = COLOR_RECT_A
--- scenes/player_camera/player_camera.gd
+++ scenes/player_camera/player_camera.gd
@@ -9,6 +9,15 @@ const SHAKE_DIMINISH_SPEED := 200.0
 @export var player: Player
 
 var _current_shake_magnitude := 5.0
+var _registed_lock_areas: Array[CameraLockArea] = []
+
+
+func register_lock_area(lock_area: CameraLockArea) -> void:
+	_registed_lock_areas.append(lock_area)
+
+
+func unregister_lock_area(lock_area: CameraLockArea) -> void:
+	_registed_lock_areas.erase(lock_area)
 
 
 func _ready() -> void:
@@ -16,7 +25,24 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	global_position = player.global_position + player.orientation * ORIENTATION_OFFSET
+	var target_x
+	var target_y
+
+	for lock_area in _registed_lock_areas:
+		if lock_area.lock_x:
+			target_x = lock_area.global_position.x
+		if lock_area.lock_y:
+			target_y = lock_area.global_position.y
+
+	var target_position_from_player = (
+		player.global_position + player.orientation * ORIENTATION_OFFSET
+	)
+	if target_x == null:
+		target_x = target_position_from_player.x
+	if target_y == null:
+		target_y = target_position_from_player.y
+
+	global_position = Vector2(target_x, target_y)
 
 	if _current_shake_magnitude > 0:
 		var shake_offset_x = randf_range(-_current_shake_magnitude, _current_shake_magnitude)
