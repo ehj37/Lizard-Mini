b858426ef453305156995fe05eb6c1ceffa37fe0
Evan Johnson <johnsonev207@gmail.com>
2026-01-03 08:48:33 -0800

    Tweak explosion visual, add dust clouds on door open, tweak door open sound

--- scenes/wall/door/door.gd
+++ scenes/wall/door/door.gd
@@ -4,9 +4,12 @@ extends StaticBody2D
 
 @export var progress_indicator: ProgressIndicator
 
+@onready var dust_cloud_resource := preload("./door_dust_cloud/door_dust_cloud.tscn")
 @onready var sprite: Sprite2D = $ColorRect/Sprite2D
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 @onready var interact_area: InteractArea = $InteractArea
+@onready var right_dust_spawn: Marker2D = $RightDustSpawn
+@onready var left_dust_spawn: Marker2D = $LeftDustSpawn
 
 
 func _ready() -> void:
@@ -16,6 +19,15 @@ func _ready() -> void:
 
 func _unlock() -> void:
 	AudioManager.play_effect_at(global_position, SoundEffectConfiguration.Type.DOOR_DOWN)
+
+	var right_dust_cloud = dust_cloud_resource.instantiate() as Sprite2D
+	right_dust_cloud.global_position = right_dust_spawn.global_position
+	owner.add_child(right_dust_cloud)
+
+	var left_dust_cloud = dust_cloud_resource.instantiate() as Sprite2D
+	left_dust_cloud.flip_h = true
+	left_dust_cloud.global_position = left_dust_spawn.global_position
+	owner.add_child(left_dust_cloud)
 
 	var sprite_pos_tween = get_tree().create_tween()
 	sprite_pos_tween.tween_property(sprite, "position:y", 86, 1.5).set_trans(Tween.TRANS_CUBIC)
