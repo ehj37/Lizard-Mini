32aef6a18dd0070a156c40715cd54e0515e267d7
Evan Johnson <johnsonev207@gmail.com>
2026-04-04 09:08:12 -0700

    Tweak player sword swing + connect sound effects, prevent invalid sound effect limit

--- audio/sound_effect_config.gd
+++ audio/sound_effect_config.gd
@@ -1,3 +1,5 @@
+@tool
+
 class_name SoundEffectConfig
 
 extends Resource
@@ -6,7 +8,17 @@ enum LimitBehavior { REPLACE_OLD, NOOP }
 
 # The max number of times this effect can be played at once. Set to -1 if there
 # should not be a limit.
-@export var limit: int = 1
+@export var limit: int = 1:
+	set(new_value):
+		if new_value == 0:
+			push_error("Cannot set limit to 0")
+			return
+
+		if new_value < -1:
+			push_error("Cannot set value to value less than -1.")
+			return
+
+		limit = new_value
 @export var limit_behavior: LimitBehavior = LimitBehavior.REPLACE_OLD
 @export var pitch_variance: float = 0.0
 @export var audio_stream: AudioStreamOggVorbis
