8edc37ee4f65110267c45cd7eee5f62e8d873b69
Evan Johnson <johnsonev207@gmail.com>
2026-04-28 21:19:34 -0700

    Improve visuals for collapsing floor, toggleable floor, and hold switch

--- scenes/floor/collapsing_floor/collapsing_floor.gd
+++ scenes/floor/collapsing_floor/collapsing_floor.gd
@@ -2,43 +2,32 @@ class_name CollapsingFloor
 
 extends StaticBody2D
 
-const TIME_BEFORE_COLLAPSE: float = 0.65
-const TIME_COLLAPSED: float = 0.8
-const TIME_TO_REFORM: float = 0.3
+var _is_player_on: bool = false
 
-var _can_collapse: bool = true
-
-@onready var detection_area: Area2D = $Area2D
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var collision_shape: CollisionShape2D = $CollisionShape2D
-@onready var color_rect: ColorRect = $ColorRect
+@onready var _animation_player: AnimationPlayer = $AnimationPlayer
 
 
-func _on_area_2d_body_entered(_body: Node2D) -> void:
-	if _can_collapse:
-		detection_area.set_deferred("monitoring", false)
-		_collapse()
+func _ready() -> void:
+	_animation_player.play("reform")
+	_animation_player.seek(_animation_player.get_animation("reform").length)
 
 
-func _collapse() -> void:
-	_can_collapse = false
-	color_rect.color = Color.WHITE
-	var initial_flash_tween: Tween = get_tree().create_tween()
-	initial_flash_tween.tween_property(color_rect, "color", Color.BLUE, TIME_BEFORE_COLLAPSE)
-	await get_tree().create_timer(TIME_BEFORE_COLLAPSE).timeout
+func _collapse_and_reform() -> void:
+	_animation_player.play("collapse")
+	await _animation_player.animation_finished
 
-	modulate.a = 0.5
-	collision_shape.disabled = true
-	await get_tree().create_timer(TIME_COLLAPSED).timeout
+	_animation_player.play("reform")
+	await _animation_player.animation_finished
 
-	_reform()
+	if _is_player_on:
+		_collapse_and_reform()
 
 
-func _reform() -> void:
-	var alpha_tween: Tween = get_tree().create_tween()
-	alpha_tween.tween_property(self, "modulate:a", 1.0, TIME_TO_REFORM)
-	await get_tree().create_timer(TIME_TO_REFORM).timeout
+func _on_player_detection_area_body_entered(_player: Player) -> void:
+	_is_player_on = true
+	if !_animation_player.is_playing():
+		_collapse_and_reform()
 
-	_can_collapse = true
-	detection_area.set_deferred("monitoring", true)
-	collision_shape.disabled = false
+
+func _on_player_detection_area_body_exited(_player: Player) -> void:
+	_is_player_on = false
--- scenes/objects/hold_switch/hold_switch.gd
+++ scenes/objects/hold_switch/hold_switch.gd
@@ -4,6 +4,9 @@ extends Node2D
 
 signal interaction_complete
 
+@onready var _animation_player: AnimationPlayer = $AnimationPlayer
+
 
 func _on_interact_area_interaction_complete() -> void:
+	_animation_player.play("retract")
 	interaction_complete.emit()
